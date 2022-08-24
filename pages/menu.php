41024b7cb392c0b0e4ff955d9496b9dbbc9ce529
Klimitenko Vlad <klimitenko43@gmail.com>
2022-08-24 21:07:32 +0500

    Add page 44

--- pages/menu.php
+++ pages/menu.php
@@ -1,22 +1,27 @@
 <ul class="nav nav-tabs nav-justified">
-<li <?php echo ($page==1)? "class='active'":"" ?>>
-<a href="index.php?page=1">Tours</a></li>
-<li <?php echo ($page==2)? "class='active'":"" ?>>
-<a href="index.php?page=2">Comments</a></li>
-<li <?php echo ($page==3)? "class='active'":"" ?>>
-<a href="index.php?page=3">Registration</a></li>
-<li <?php echo ($page==4)? "class='active'":"" ?>>
-<a href="index.php?page=4">Admin Forms</a></li>
-<li <?php echo ($page==6)? "class='active'":"" ?>>
-<a href="index.php?page=6">Private</a>
-</li>
-<li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Name</a>
-          <a class="dropdown-item" href="#">Evgenij</a>
-        </div>
-      </li>
+  <li <?php echo ($page == 1) ? "class='active'" : "" ?>>
+    <a href="index.php?page=1">Tours</a>
+  </li>
+  <li <?php echo ($page == 2) ? "class='active'" : "" ?>>
+    <a href="index.php?page=2">Comments</a>
+  </li>
+  <li <?php echo ($page == 3) ? "class='active'" : "" ?>>
+    <a href="index.php?page=3">Registration</a>
+  </li>
+  <li <?php echo ($page == 4) ? "class='active'" : "" ?>>
+    <a href="index.php?page=4">Admin Forms</a>
+  </li>
+  <li <?php echo ($page == 6) ? "class='active'" : "" ?>>
+    <a href="index.php?page=6">Private</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Dropdown link
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+      <a class="dropdown-item" href="#">Name</a>
+      <a class="dropdown-item" href="#">Evgenij</a>
+      <a class="dropdown-item" href="#">Vlad</a>
+    </div>
+  </li>
 </ul>
